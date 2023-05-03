eb3c597c2cc044cab927fccd01824caa01ba753d
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2023-05-02 19:27:59 -0600

    Initial Commit

---
+++ ReadJSON/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ReadJSON
+//
+//  Created by Luis Enrique Rosas Espinoza on 02/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReadJSON/ReadJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadJSONApp.swift
+//  ReadJSON
+//
+//  Created by Luis Enrique Rosas Espinoza on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
